ee5a1c7d28013c958503289cd776bebbba3d58dd
akryga <akryga@gmail.com>
2024-01-26 17:08:59 +0300

    first commit

---
+++ src/main/java/ru/bearhill/activiti/SampleCloudConnectorApplication.java
@@ -0,0 +1,13 @@
+package ru.bearhill.activiti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleCloudConnectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleCloudConnectorApplication.class, args);
+	}
+
+}
